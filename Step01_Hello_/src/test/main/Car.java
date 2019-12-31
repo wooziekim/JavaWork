dc69faff4eecc1f055e2a3f5d69cd184313a720f
kimtaewoo <wooziekim@gmail.com>
2019-12-31 10:18:47 +0900

    Car 클래스를 만들고 달렸어요

---
+++ Step01_Hello_/src/test/main/Car.java
@@ -0,0 +1,9 @@
+package test.main;
+
+public class Car {
+	public void drive() {
+		System.out.println("달려요");
+		
+		
+	}
+}
--- Step01_Hello_/src/test/main/MainClass01.java
+++ Step01_Hello_/src/test/main/MainClass01.java
@@ -4,6 +4,8 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("김구라");
 		System.out.println("원숭이");
+		Car car1=new Car();
+		car1.drive();
 	}
 }
 

43579fb415708f07666d937dd647d26aef2a67bc
kimtaewoo <wooziekim@gmail.com>
2019-12-31 11:07:25 +0900

    ccccc

--- Step01_Hello_/src/test/main/MainClass01.java
+++ Step01_Hello_/src/test/main/MainClass01.java
@@ -1,11 +1,21 @@
 package test.main;
 
+import test.mypac.Computer;
+import test.mypac.Phone;
+
 public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("김구라");
 		System.out.println("원숭이");
 		Car car1=new Car();
 		car1.drive();
+		System.out.println("주뎅이");
+		Phone p1=new Phone();
+		p1.call();
+		p1.takePicture();
+		
+		new Computer().playGame();
+		
 	}
 }
 
